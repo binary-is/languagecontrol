6f1cd39ed8b3b812e3e76ded4c0cfef1cce15985
Helgi Hrafn Gunnarsson <helgi@binary.is>
2019-08-22 22:22:52 +0000

    Non-functional changes for compatibility with Django 2.x.

--- middleware.py
+++ middleware.py
@@ -1,4 +1,10 @@
 class LanguageControlMiddleware():
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
     def process_request(self, request):
         if request.META.has_key('HTTP_ACCEPT_LANGUAGE'):
             del request.META['HTTP_ACCEPT_LANGUAGE']
