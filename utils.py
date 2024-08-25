89a076a10d67a015a0ba2505a615687b05f7b1e9
Helgi Hrafn Gunnarsson <helgi@binary.is>
2024-08-25 13:44:55 +0000

    Update for Django 5.x compatibility.

--- utils.py
+++ utils.py
@@ -3,4 +3,4 @@ from django.utils import translation
 
 def set_language(request, language):
     translation.activate(language)
-    request.session[translation.LANGUAGE_SESSION_KEY] = language
+    request.session["django_language"] = language
